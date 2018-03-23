b8d787f25dfbe875ef4f33827e59fe06d97d67ed
will.chen <will.chen@grandsys.com>
2018-03-23 13:00:09 +0800

    fix stdio error

--- src/nimeServer.js
+++ src/nimeServer.js
@@ -2,11 +2,8 @@
 
 const debug = require('debug')('nime:server');
 const stdio = require('stdio');
-const { promisify } = require('util');
 
 const { initService, handleRequest } = require('./requestHandler');
-
-stdio.readByLines = promisify(stdio.readByLines);
 
 const createServer = async (services = []) => {
   const connections = {};
@@ -57,40 +54,42 @@ const createServer = async (services = []) => {
   };
 
   const listen = async () => {
-    try {
-      const line = await stdio.readByLines();
-      line = line.trim();
-      const parts = line.split('|', 2);
+    stdio.readByLines(
+      function lineHandler(line) {
+        line = line.trim();
+        const parts = line.split('|', 2);
 
-      if (parts.length == 2) {
-        const clientId = parts[0];
-        const msgText = parts[1];
-        const msg = JSON.parse(msgText);
-        debug('msg: ', msgText);
-        let client;
+        if (parts.length == 2) {
+          const clientId = parts[0];
+          const msgText = parts[1];
+          const msg = JSON.parse(msgText);
+          let client;
 
-        if (!connections.hasOwnProperty(clientId)) {
-          client = { service: null, state: null };
-          connections[clientId] = client;
-          debug('new client', clientId);
+          if (!connections.hasOwnProperty(clientId)) {
+            client = { service: null, state: null };
+            connections[clientId] = client;
+            debug('new client', clientId);
+          }
+
+          if (msg['method'] === 'close') {
+            // special handling for closing a client
+            removeClient(clientId);
+            debug('client disconnected:', clientId + '\n');
+          } else {
+            handleClientRequest(clientId, msg).then(ret => {
+              // Send the response to the client via stdout
+              // one response per line in the format "PIME_MSG|<client_id>|<json reply>"
+              const reply_line =
+                'PIME_MSG|' + clientId + '|' + JSON.stringify(ret) + '\n';
+              process.stdout.write(reply_line);
+            });
+          }
         }
-
-        if (msg['method'] === 'close') {
-          // special handling for closing a client
-          removeClient(clientId);
-          debug('client disconnected:', clientId + '\n');
-        } else {
-          const ret = await handleClientRequest(clientId, msg);
-          // Send the response to the client via stdout
-          // one response per line in the format "PIME_MSG|<client_id>|<json reply>"
-          const reply_line =
-            'PIME_MSG|' + clientId + '|' + JSON.stringify(ret) + '\n';
-          process.stdout.write(reply_line);
-        }
+      },
+      function(err) {
+        console.log('Finished');
       }
-    } catch (error) {
-      console.log('finished');
-    }
+    );
   };
   return { listen };
 };
